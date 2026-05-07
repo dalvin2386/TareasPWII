db65eb244db84214a7454fa75eac8262d0a4de31
dalvin2386 <149994200+dalvin2386@users.noreply.github.com>
2026-05-06 22:42:13 -0600

    tests
    haciendo pruebas de funcionamiento y agregando un poco estilo

--- TareaAmbienteDesarrollo/index.js
+++ TareaAmbienteDesarrollo/index.js
@@ -1,8 +1,8 @@
 var colors = require("colors");
 const CalculadoraEnvio = require("./src/CalculadoraEnvio");
 
-const peso = 7;
-const distancia = 52;
+const peso = 5;
+const distancia = 50;
 
 console.log("==================================".green);
 console.log("  Calculadora de Costo de Envío".green);
@@ -11,3 +11,6 @@ console.log("");
 
 const envio = new CalculadoraEnvio(peso, distancia);
 envio.calcular();
+
+console.log("".green);
+console.log("==================================".green);
